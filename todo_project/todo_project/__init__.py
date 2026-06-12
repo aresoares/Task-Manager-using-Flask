4d04ee79f5503e4aec7b5d4e7932760af5aef76b
aresoares <areth17@gmail.com>
2026-06-12 19:31:44 -0300

    corrigindo achados de seguranca da etapa 4

--- todo_project/todo_project/__init__.py
+++ todo_project/todo_project/__init__.py
@@ -1,3 +1,6 @@
+import os
+import secrets
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -5,15 +8,5 @@ from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '45cf93c4d41348cd9980674ade9a7356'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or secrets.token_hex(32)
 db = SQLAlchemy(app)
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' 
-login_manager.login_message_category = 'danger'
-
-bcrypt = Bcrypt(app)
-
-# Always put Routes at end
-from todo_project import routes
